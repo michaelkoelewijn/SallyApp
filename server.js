dfeeebac50c1631c51ee14712f3b1a04663c6042
Michael Koelewijn <michael.koelewijn@wearejust.com>
2017-10-09 17:40:00 +0200

    Chore: Change date format in object save

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ main.prepare()
       let currTime = new Date()
       let timeInSeconds = Math.ceil((currTime.getTime() - serverTime.getTime()) / 1000) - 5
       let scoreData = { 'name': data.name, 'time': timeInSeconds }
-      let dateString = `${currTime.getDate()}-${currTime.getMonth() + 1 }-${currTime.getFullYear()}`
+      let dateString = `${currTime.getFullYear()}-${currTime.getMonth() + 1 }-${currTime.getDate()}`
+
       allScores[client.id] = scoreData
       
       //Save to Google Firebase
