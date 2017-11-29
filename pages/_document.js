35e095f995b271057cb42ec1ff21c195dc36c8c6
Rutger Kooijman <rutger.kooijman@wearejust.com>
2017-11-29 16:43:53 +0100

    Bugfix: added closing  slash to meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
                         <title>Sally App Sally Down</title>
                     </Head>
                     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui" />
-                    <meta name="apple-mobile-web-app-capable" content="yes">
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
                     <meta name="theme-color" content="#000" />
                     <link rel="manifest" href="" />
                     <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed" rel="stylesheet" />
